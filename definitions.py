4a7a9b9a4e687f708516d04ff651cbb1910046e8
su530201 <olivier.kaufmann@umons.ac.be>
2021-10-24 18:23:57 +0200

    Adds the reset_config.sh script and config.default file to enable resetting the configuration file; improves the definition.py file by adding function to retrieve version and release from git tags

--- definitions.py
+++ definitions.py
@@ -1,9 +1,19 @@
 from pathlib import Path
 
+def get_version():
+    cmd = ['git', 'describe', '--tags', '--abbrev=0']
+    output = check_output(cmd)
+    return output.decode('latin1').strip()
+    
+def get_release():
+    cmd = ['git', 'describe', '--tags']
+    output = check_output(cmd)
+    return output.decode('latin1').strip()
+
 PROJECT = 'nQb8or'
 AUTHOR = 'O. KAUFMANN'
-VERSION = u'0.2'
-RELEASE = VERSION + u'.1a'
+VERSION = get_version()
+RELEASE = get_release()
 LICENSE = 'GPLv3'
 SHORT_DESCRIPTION = 'nQb8or (pronounced as "incubator") is a way to quickly setup an environment to develop a package' \
                     ' or application with python under linux using bash shell scripts'
--- dynamic_components/definitions.py
+++ dynamic_components/definitions.py
@@ -1,9 +1,19 @@
 from pathlib import Path
 
+def get_version():
+    cmd = ['git', 'describe', '--tags', '--abbrev=0']
+    output = check_output(cmd)
+    return output.decode('latin1').strip()
+    
+def get_release():
+    cmd = ['git', 'describe', '--tags']
+    output = check_output(cmd)
+    return output.decode('latin1').strip()
+
 PROJECT = u'{project}'
 AUTHOR = u'{author}'
-VERSION = u'{version}'
-RELEASE = VERSION + u'.1a'
+VERSION = get_version()
+RELEASE = get_release()
 LICENSE = u'GPLv3'
 SHORT_DESCRIPTION = u'@@@ Please insert a short description here...'
 ROOT_DIR = Path(__file__).parent.absolute()
